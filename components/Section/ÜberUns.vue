0ad4070053946f881f3269f75ad4868c401bdf99
Konstantin <64841866+KonstntinM@users.noreply.github.com>
2021-07-11 14:55:47 +0200

    neuen über uns text, plausible event logs und debug logs entferen

--- components/Section/ÜberUns.vue
+++ components/Section/ÜberUns.vue
@@ -4,7 +4,7 @@
         <h3 class="w-full text-4xl font-extrabold tracking-normal text-gray-900 sm:text-5xl md:text-5xl md:pr-10 lg:pr-16 xl:pr-20 md:leading-none md:-mt-2 md:w-1/2">Über uns</h3>
         <div class="flex flex-col w-full mt-8 space-y-5 md:w-1/2 md:space-y-10 md:mt-0">
             <p class="col-span-6 text-base font-normal text-gray-700 lg:leading-8 xl:leading-8 md:text-xl">
-                Warum wählen wir eigentlich nicht schon mit 14 den Bundestag, was ist eigentlich eine EU-Verordnung und wie gründet man eine Partei? Diese und ähnliche Fragen interessieren und beschäftigen uns in unserem Alltag. Doch leider benötigt es viel Zeit sich mit Politik und Gesellschaft umfassend auseinanderzusetzen. Seit vergangenem Herbst arbeiten wir gemeinsam daran eine neue Jugendredaktion zu starten, um das zu ändern und crossmedial Videos mit einem neuen Blickwinkel für Jugendliche in unserem Alter produzieren.
+                An Berichterstattung zu aktueller Politik mangelt es nicht im Internet. Hunderte Nachrichtenseiten kämpfen mit YouTube-Videos und Blogs um die Aufmerksamkeit der Nutzer:innen. Doch eine Sache hat uns bisher gefehlt: Die Perspektive junger Menschen. Deshalb haben wir uns entschieden unsere eigene Jugendredaktion zu gründen, darüber zu sprechen, was uns bewegt und dort zu berichten, wo wir uns sowieso online aufhalten. Auf YouTube, Instagram und Twitter. Dabei ist unser Anspruch qualitativ hochwertigen Content zu produzieren, der mit anderem Angebot im Netz mithalten kann.
             </p>
 
             <p class="col-span-6 text-base font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl">
